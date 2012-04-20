bf4fdee2523644f045f1e4c07aa42f4ca132923e
macrohuang <macrohuang@126.com>
2012-04-21 00:10:34 +0800

    remove files no longer use

--- src/main/java/com/macrohuang/mongo/MongoDBFactory.java
+++
@@ -1,8 +0,0 @@
-package com.macrohuang.mongo;
-
-import com.macrohuang.mongo.exception.MongoDataAccessException;
-import com.mongodb.DB;
-
-public interface MongoDBFactory {
-	public abstract DB getDB(String dbName) throws MongoDataAccessException;
-}
--- src/main/java/com/macrohuang/mongo/factory/DefaultMongoDBFactory.java
+++
@@ -1,7 +0,0 @@
-package com.macrohuang.mongo.factory;
-
-
-public class DefaultMongoDBFactory extends AbstractMongoDBFactory {
-
-
-}

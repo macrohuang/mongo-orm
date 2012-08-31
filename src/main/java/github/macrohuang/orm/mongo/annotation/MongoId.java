93c128de11de68bd8a11bcae5765d05d8b5842ae
Macro Huang <macrohuang@126.com>
2012-08-31 17:31:53 +0800

    add document

--- src/main/java/github/macrohuang/orm/mongo/annotation/MongoId.java
+++ src/main/java/github/macrohuang/orm/mongo/annotation/MongoId.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Used in the _id filed, instead of ObjectId, it must be an String, with the
+ * value of ObjectId.
+ * 
+ * @author Macro Huang
+ * 
+ */
 @Target(value = ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MongoId {

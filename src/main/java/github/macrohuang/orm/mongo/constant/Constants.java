d5f77871ab0c356f7e4c34a31b232741c44a743c
Macro Huang <macrohuang@126.com>
2012-11-23 11:34:14 +0800

    rename orm to odm, actualy, it is an Object-Document-Mapping not an Object-Relaction-Mapping

--- src/main/java/github/macrohuang/orm/mongo/constant/Constants.java
+++
@@ -1,8 +0,0 @@
-package github.macrohuang.orm.mongo.constant;
-
-public class Constants {
-	public static final String MONGO_ID = "_id";
-	public static final int SORT_MAX_RECORD_WITHOUT_INDEX = 5000;
-	public static boolean convertLogEnable = false;
-	public static boolean coreLogEnable = true;
-}
--- src/main/java/github/macrohuang/orm/mongo/factory/MongoDBFactory.java
+++
@@ -1,9 +0,0 @@
-package github.macrohuang.orm.mongo.factory;
-
-import github.macrohuang.orm.mongo.exception.MongoDataAccessException;
-
-import com.mongodb.DB;
-
-public interface MongoDBFactory {
-	public abstract DB getDB(String dbName) throws MongoDataAccessException;
-}
